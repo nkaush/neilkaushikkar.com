a679933357b3df5bb42e08474716a8cb17499662
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-02-06 12:44:05 -0600

    Add support for global favicon and fix static url bugs'

--- src/filters.rs
+++ src/filters.rs
@@ -4,8 +4,16 @@ use warp::Filter;
 /// combine all routes
 pub fn all_routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     index()
+        .or(home())
         .or(serve_static())
         .or(resume())
+        .or(favicon())
+}
+
+pub fn home() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path::end()
+        .and(warp::get())
+        .and_then(handlers::index)
 }
 
 pub fn index() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -20,4 +28,8 @@ pub fn serve_static() -> impl Filter<Extract = impl warp::Reply, Error = warp::R
 
 pub fn resume() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path("resume").and(warp::fs::file("www/static/doc/resume.pdf"))
+}
+
+pub fn favicon() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("favicon.ico").and(warp::fs::file("www/static/images/favicon.png"))
 }
