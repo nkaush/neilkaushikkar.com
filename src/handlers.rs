91440b23dee035213115713048be7096c047a18b
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-03-16 11:48:55 -0700

    Speed up content delivery using lazy_static handlebars template allocation

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,25 +2,31 @@ use std::convert::Infallible;
 use warp::http::StatusCode;
 use handlebars::Handlebars;
 use serde_json::{Value, Map};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref HANDLEBARS: Handlebars<'static> = {
+        let mut h = Handlebars::new();
+        h.register_template_file("index-template", "templates/index.hbs").unwrap();
+        h.register_template_file("skills", "templates/sections/skills.hbs").unwrap();
+        h.register_template_file("timeline", "templates/sections/timeline.hbs").unwrap();
+        h.register_template_file("portfolio", "templates/sections/portfolio.hbs").unwrap();
+        h.register_template_file("affiliations", "templates/sections/affiliations.hbs").unwrap();
+        // h.register_template_file("portfolio_modals", "templates/sections/portfolio_modals.htmls").unwrap();
+
+        h
+    };
+
+    static ref BODY: String = {
+        let json: String = std::fs::read_to_string("content.json").unwrap();
+        let parsed: Value = serde_json::from_str(&json).unwrap();
+        let map: &Map<String, Value> = parsed.as_object().unwrap();
+
+        HANDLEBARS.render("index-template", map).unwrap()
+    };
+}
 
 /// GET /index
-pub async fn index() -> Result<impl warp::Reply, Infallible> {
-    let mut handlebars = Handlebars::new();
-    handlebars.register_template_file("index-template", "templates/index.hbs").unwrap();
-
-    // register partials
-    handlebars.register_template_file("skills", "templates/sections/skills.hbs").unwrap();
-    handlebars.register_template_file("timeline", "templates/sections/timeline.hbs").unwrap();
-    handlebars.register_template_file("portfolio", "templates/sections/portfolio.hbs").unwrap();
-    handlebars.register_template_file("affiliations", "templates/sections/affiliations.hbs").unwrap();
-
-    // handlebars.register_template_file("portfolio_modals", "templates/sections/portfolio_modals.htmls").unwrap();
-
-    let config_data = std::fs::read_to_string("content.json").unwrap();
-
-    let parsed: Value = serde_json::from_str(&config_data).unwrap();
-    let obj: Map<String, Value> = parsed.as_object().unwrap().clone();
-    let body: String = handlebars.render("index-template", &obj).unwrap();
-    
-    Ok(warp::reply::with_status(warp::reply::html(body), StatusCode::OK))
+pub async fn index() -> Result<impl warp::Reply, Infallible> {    
+    Ok(warp::reply::with_status(warp::reply::html(BODY.clone()), StatusCode::OK))
 }
