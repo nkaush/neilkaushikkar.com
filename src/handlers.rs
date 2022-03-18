0ce8f4052c497bbd96faa289353f316a558e4d59
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-03-18 11:08:01 -0700

    Style tweaks

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,7 +5,7 @@ use serde_json::{Value, Map};
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref HANDLEBARS: Handlebars<'static> = {
+    static ref BODY: String = {
         let mut h = Handlebars::new();
         h.register_template_file("index-template", "templates/index.hbs").unwrap();
         h.register_template_file("skills", "templates/sections/skills.hbs").unwrap();
@@ -14,15 +14,11 @@ lazy_static! {
         h.register_template_file("affiliations", "templates/sections/affiliations.hbs").unwrap();
         // h.register_template_file("portfolio_modals", "templates/sections/portfolio_modals.htmls").unwrap();
 
-        h
-    };
-
-    static ref BODY: String = {
         let json: String = std::fs::read_to_string("content.json").unwrap();
         let parsed: Value = serde_json::from_str(&json).unwrap();
         let map: &Map<String, Value> = parsed.as_object().unwrap();
 
-        HANDLEBARS.render("index-template", map).unwrap()
+        h.render("index-template", map).unwrap()
     };
 }
 
