91b84d6806e72a0056207eb7aa8681983c39238c
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-08-19 11:01:06 -0700

    Refactor to use template extension

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,38 +1,37 @@
+use warp::{Reply, reply, Rejection, reject, http::StatusCode, filters::body};
 use std::convert::Infallible;
-use warp::{Rejection, http::StatusCode};
-use handlebars::Handlebars;
 use serde_json::{Value, Map};
+use handlebars::Handlebars;
 use serde::Serialize;
-use lazy_static::lazy_static;
 
-lazy_static! {
+lazy_static::lazy_static! {
+    static ref DESERIALIZED_DATA: Value = {
+        let json: String = std::fs::read_to_string("content.json").unwrap();
+        serde_json::from_str(&json).unwrap()
+    };
+
     static ref HOME: String = {
         let mut h = Handlebars::new();
         h.register_template_file("index", "templates/index.hbs").unwrap();
-
-        let json: String = std::fs::read_to_string("content.json").unwrap();
-        let parsed: Value = serde_json::from_str(&json).unwrap();
-        let map: &Map<String, Value> = parsed.as_object().unwrap();
+        h.register_template_file("base", "templates/base.hbs").unwrap();
+        let map: &Map<String, Value> = DESERIALIZED_DATA.as_object().unwrap();
 
         h.render("index", map).unwrap()
     };
 
     static ref E404: String = {
         let mut h = Handlebars::new();
-
         h.register_template_file("404", "templates/404.hbs").unwrap();
-
-        let json: String = std::fs::read_to_string("content.json").unwrap();
-        let parsed: Value = serde_json::from_str(&json).unwrap();
-        let map: &Map<String, Value> = parsed.as_object().unwrap();
+        h.register_template_file("base", "templates/base.hbs").unwrap();
+        let map: &Map<String, Value> = DESERIALIZED_DATA.as_object().unwrap();
 
         h.render("404", map).unwrap()
     };
 }
 
 /// GET /index
-pub async fn index() -> Result<impl warp::Reply, Infallible> {    
-    Ok(warp::reply::with_status(warp::reply::html(HOME.clone()), StatusCode::OK))
+pub async fn index() -> Result<impl Reply, Infallible> {    
+    Ok(reply::with_status(reply::html(HOME.clone()), StatusCode::OK))
 }
 
 /// An API error serializable to JSON.
@@ -42,18 +41,18 @@ struct ErrorMessage {
     message: String,
 }
 
-pub async fn handle_rejection(err: Rejection) -> Result<impl warp::Reply, Infallible> {
+pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
     let code;
     let message;
 
     if err.is_not_found() {
-        return Ok(warp::reply::with_status(warp::reply::html(E404.clone()), StatusCode::NOT_FOUND));
-    } else if let Some(_e) = err.find::<warp::filters::body::BodyDeserializeError>() {
+        return Ok(reply::with_status(reply::html(E404.clone()), StatusCode::NOT_FOUND));
+    } else if let Some(_e) = err.find::<body::BodyDeserializeError>() {
         // This error happens if the body could not be deserialized correctly
         // We can use the cause to analyze the error and customize the error message
         message = "BAD_REQUEST";
         code = StatusCode::BAD_REQUEST;
-    } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
+    } else if let Some(_) = err.find::<reject::MethodNotAllowed>() {
         // We can handle a specific error, here METHOD_NOT_ALLOWED,
         // and render it however we want
         code = StatusCode::METHOD_NOT_ALLOWED;
@@ -70,9 +69,9 @@ pub async fn handle_rejection(err: Rejection) -> Result<impl warp::Reply, Infall
         message: message.into(),
     };
 
-    let reply = warp::reply::html(
+    let reply = reply::html(
         serde_json::to_string(&error).unwrap()
     );
 
-    Ok(warp::reply::with_status(reply, code))
+    Ok(reply::with_status(reply, code))
 }
